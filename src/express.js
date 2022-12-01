76d2fcb1c9d5ee53160ef1ae60958b93f38543ef
Dhairya Khara <kharadhairya@gmail.com>
2022-11-30 22:45:41 -0500

    fixed bug

--- src/express.js
+++ src/express.js
@@ -18,17 +18,16 @@ app.use(cors())
 app.use(express.static(clientDirectory))
 
 
-//serving up build directory
-app.get('*', (req, res) => {
-    res.sendFile(path.join(clientDirectory, '/index.html'))
-});
-
-
 app.use(createUserController)
 app.use(loginUserController)
 app.use(uploadTranscriptController)
 app.use(getIntentsController)
 app.use(getOneTranscriptIntentsController)
 app.use(getTranscriptsController)
+
+//serving up build directory
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientDirectory, '/index.html'))
+});
 
 module.exports = app
